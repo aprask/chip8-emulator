1f593b31a0e8a40cff4e88acd391a0dadb7ee3bb
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2024-11-04 07:10:20 -0500

    Updated chip8 keypad w/ logs

--- src/chip8_textures.h
+++ src/chip8_textures.h
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "chip8.h"
-void sdl_init(char const* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight);
+void sdl_init(const char* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight);
 void sdl_destroyer();
 void sdl_update(void const* buffer, int pitch);
 bool process_input(chip8* chip8_t);
